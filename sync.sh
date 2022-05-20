3e5d21b99bc519c6ddb967d0e2f44175f2d5ad02
Curt Bushko <cbushko@gmail.com>
2022-05-20 11:54:06 -0400

    get a decent stub of the layout working. mirrors consul-k8s/control-plane

---
+++ sync.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+SRC=${HOME}/workspace/github.com/hashicorp/feature-cni
+DEST=${HOME}/workspace/github.com/curtbushko/cni-poc
+
+mkdir -p "${DEST}/subcommand"
+#mkdir -p "${DEST}/connect-inject"
+
+rsync -a "${SRC}/control-plane/cni" "${DEST}"
+#rsync -a "${SRC}/control-plane/connect-inject" "${DEST}"
+rsync "${SRC}/control-plane/Dockerfile" "${DEST}"
+rsync "${SRC}/control-plane/Makefile" "${DEST}"
+rsync "${SRC}/control-plane/go.mod" "${DEST}"
+rsync "${SRC}/control-plane/go.sum" "${DEST}"
+rsync -a "${SRC}/control-plane/subcommand/cni-install" "${DEST}/subcommand/cni-install"
+rsync -a "${SRC}/control-plane/subcommand/cni-validate" "${DEST}/subcommand/cni-validate"
+
