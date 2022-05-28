f1699ec7fbb61b90d406046f6b6b9a541c52c028
Curt Bushko <cbushko@gmail.com>
2022-05-28 15:09:48 -0400

    build-support scripts changed

--- pull-sync.sh
+++ pull-sync.sh
@@ -3,6 +3,7 @@ SRC=${HOME}/workspace/github.com/hashicorp/feature-cni
 DEST=${HOME}/workspace/github.com/curtbushko/cni-poc
 
 mkdir -p "${DEST}/subcommand"
+mkdir -p "${DEST}/build-support/scripts"
 #mkdir -p "${DEST}/connect-inject"
 
 rsync -a "${SRC}/control-plane/cni" "${DEST}"
@@ -12,4 +13,5 @@ rsync "${SRC}/control-plane/Makefile" "${DEST}"
 rsync "${SRC}/control-plane/go.mod" "${DEST}"
 rsync "${SRC}/control-plane/go.sum" "${DEST}"
 rsync -a "${SRC}/control-plane/subcommand/cni-install" "${DEST}/subcommand/cni-install"
+rsync -a "${SRC}/control-plane/build-support/scripts/build-local.sh" "${DEST}/build-support/scripts"
 
