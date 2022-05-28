0cafe4b7c62afa8e51e3257d5b3d800bfb9a8213
Curt Bushko <cbushko@gmail.com>
2022-05-28 14:21:08 -0400

    update push-sync.sh script

--- push-sync.sh
+++ push-sync.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-DEST=${HOME}/workspace/github.com/hashicorp/feature-cni
+DEST=${HOME}/workspace/github.com/hashicorp/feature-cni/control-plane
 SRC=${HOME}/workspace/github.com/curtbushko/cni-poc
 
-mkdir -p "${DEST}/subcommand"
+mkdir -p "${DEST}/subcommand/cni-install"
+mkdir -p "${DEST}/cni"
 #mkdir -p "${DEST}/connect-inject"
 
-rsync -a "${SRC}/control-plane/cni" "${DEST}"
+rsync -a "${SRC}/cni" "${DEST}"
 #rsync -a "${SRC}/control-plane/connect-inject" "${DEST}"
-rsync "${SRC}/control-plane/Dockerfile" "${DEST}"
-rsync "${SRC}/control-plane/Makefile" "${DEST}"
-rsync "${SRC}/control-plane/go.mod" "${DEST}"
-rsync "${SRC}/control-plane/go.sum" "${DEST}"
-rsync -a "${SRC}/control-plane/subcommand/cni-install" "${DEST}/subcommand/cni-install"
+rsync "${SRC}/Dockerfile" "${DEST}"
+rsync "${SRC}/Makefile" "${DEST}"
+rsync "${SRC}/go.mod" "${DEST}"
+rsync "${SRC}/go.sum" "${DEST}"
+rsync -a "${SRC}/subcommand/cni-install" "${DEST}/subcommand/cni-install"
 
